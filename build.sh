2d49653b3249957f5ba217d3cd5ed176dce3e25d
Zapacitu18 <irinam200320@gmail.com>
2020-08-22 23:14:24 +0300

    Fix error.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mkdir out
 make O=out clean
 export ARCH="arm64"
 export CROSS_COMPILE=~/gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=~/gcc/arm-linux-androideabi-4.9/bin/arm-linux-android-
+export CROSS_COMPILE_ARM32=~/gcc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 make O=out lolz_olive_defconfig
 make O=out -j8
 
